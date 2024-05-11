233a2eb5306cd9515af022a76e7561ea21c27a9d
Yaquta <128394765+Yaquta@users.noreply.github.com>
2024-05-11 09:29:47 +0700

    Add files via upload

---
+++ Praktikum 8/Kupu.java
@@ -0,0 +1,39 @@
+// Nama      : M Yaquta Husna
+// NIM       : 24060122130056
+// Nama File : Kupu.java
+
+class Kupu {
+    void gerak() {
+    }
+}
+
+class Ulat extends Kupu {
+    void gerak() {
+        System.out.println("ulat merayap");
+    }
+}
+
+class Kepompong extends Kupu {
+    void gerak() {
+        System.out.println("kepompong diam");
+    }
+}
+
+class KupuDewasa extends Kupu {
+    void gerak() {
+        System.out.println("kupu terbang");
+    }
+}
+
+class Data<T> {
+    private T isi;
+
+    T getIsi() {
+        return this.isi;
+    }
+
+    void setIsi(T x) {
+        this.isi = x;
+    }
+}
+
---
+++ Praktikum 8/Main.java
@@ -0,0 +1,22 @@
+// Nama      : M Yaquta Husna
+// NIM       : 24060122130056
+// Nama File : Main.java
+
+
+public class Main {
+    public static void main(String[] args) {
+        Ulat K = new Ulat();
+        Data<Kupu> anu = new Data<>();
+        
+        K.gerak();
+        
+        anu.setIsi(K);
+        anu.getIsi().gerak();
+        
+        anu.setIsi(new Kepompong());
+        anu.getIsi().gerak();
+        
+        anu.setIsi(new KupuDewasa());
+        anu.getIsi().gerak();
+    }
+}
